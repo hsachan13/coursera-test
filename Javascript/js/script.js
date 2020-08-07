de576785e4723a6bcdb90fdb87c8e09a287e8e58
hsachan13 <hsachan13@gmail.com>
2020-08-07 14:23:49 +0530

    Assignment 4 Solution.

---
+++ Javascript/js/script.js
@@ -0,0 +1,17 @@
+// var array = new Array();
+// array[0]='Himanshu';
+// array[1]=2;
+// array[2]= function (name){
+//   console.log("Hello "+ name);
+// };
+// array[3]={course:"coursera web"};
+
+// console.log(array);
+// array[2](array[0]);
+// console.log(array[3].course);
+
+
+//Short hand array creation
+
+var names=["yaakov","himanshu",{object:"course"}];
+console.log(names);
